8b2102ce70b261813be3ea04fe94dd329a4262f4
jadran22051979 <jadran.bonacic@gmail.com>
2025-10-31 21:52:26 +0100

    Napravljen 2 .zadatak

--- src/IzracunVisina.java
+++ src/IzracunVisina.java
@@ -3,14 +3,27 @@ import java.util.Scanner;
 public class IzracunVisina {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        //Definiranje broja unosa visina
         int brojVisina = 10;
+        //Polje tipa double koje ima 10 unosa
         double[] visine = new double[brojVisina];
         double suma = 0.0;
 
-        System.out.println("Unesite visinu :" + brojVisina + "ljudi u metrima");
+        System.out.println("Unesite visinu :" + brojVisina + "\tljudi u metrima");
         for (int i = 0; i < brojVisina; i++) {
+            //U ispisu će ići od visine 1
             System.out.println((i + 1) + " \t Visina : ");
+            //Punjenje polja visine tipa double s unosima
             visine[i] = Double.parseDouble(sc.nextLine());
+            //U slučaju da je visina manja od 0.0 i veća od 2.5 ispisujemo poruku
+            if (visine[i] < 0.0 || visine[i] > 2.5) {
+                System.out.println("\n Molimo unesite visinu veću od 0.0 i manju od 2.5 metara");
+                //Umanji i za 1 tj.traži ponovni unos za unos i
+                i--;
+                //Idi u slijedeću iteraciju
+                continue;
+            }
+            //Suma je zbroj visina koje će nam kasnije služiti za izračun prosjeka itd
             suma += visine[i];
         }
         //Prosječna visina
@@ -34,6 +47,25 @@ public class IzracunVisina {
             }
         }
         System.out.println("Najviša visina je = " + najvisaVisina + " metara");
+
+        //Dio za izračun studenata iznad prosječne visine
+        int brojacIznadProsjeka = 0;
+        for (int i = 1; i < brojVisina; i++) {
+            if (visine[i] > proscjenaVisina) {
+                brojacIznadProsjeka++;
+            }
+        }
+        System.out.println("Broj studenata iznad prosječne visine je = " + brojacIznadProsjeka);
+
+        //Dio za izračun studenata iznad  visine od 1.75 metara
+        int visinaManjaOd = 0;
+        double visinaTrazena = 1.75;
+        for (int i = 1; i < brojVisina; i++) {
+            if (visine[i] < visinaTrazena) {
+                visinaManjaOd++;
+            }
+        }
+        System.out.println("Broj studenata ispod  visine od 1,75 metara je = " + visinaManjaOd);
 
         sc.close();
 
